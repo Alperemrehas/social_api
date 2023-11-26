f66bee91cc5da3e379a239a8d63a2e387b3db9a7
alper <alper@mail.com>
2023-11-26 23:14:59 +0300

    Updated

--- storeapi/routers/post.py
+++ storeapi/routers/post.py
@@ -1,9 +1,14 @@
 from fastapi import APIRouter
-from storeapi.models.post import UserPost, UserPostIn
+
+from storeapi.models.post import Comment, CommentIn, UserPost, UserPostIn
 
 router = APIRouter()
 
 post_table = {}
+comment_table = {}
+
+def fin_post(post_id: int):
+    return post_table.get(post_id)
 
 @router.post("/post", response_model=UserPost)
 async def create_post(post: UserPostIn):
@@ -16,3 +21,11 @@ async def create_post(post: UserPostIn):
 @router.get("/post", response_model=list[UserPost])
 async def get_all_posts():
     return list(post_table.values())
+
+@router.post("/comment", response_model=Comment)
+async def create_comment(comment: CommentIn):
+    data = comment.model_dump()
+    last_record_id = len(comment_table)
+    new_comment = {**data, "id" : last_record_id}
+    comment_table[last_record_id] = new_comment
+    return new_comment
